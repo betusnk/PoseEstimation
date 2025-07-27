6a59ba5b5d986d13285979e11eae985e78a02a2b
Betüş <betusnko@gmail.com>
2025-07-27 13:45:53 +0300

    It works now, organized based on MediaPipe Doc.

--- poseEst.py
+++ poseEst.py
@@ -5,6 +5,7 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from mediapipe.framework.formats import landmark_pb2
 from mediapipe import solutions
+import time
 
 def draw_landmarks_on_image(rgb_image, detection_result):
   pose_landmarks_list = detection_result.pose_landmarks
@@ -24,23 +25,49 @@ def draw_landmarks_on_image(rgb_image, detection_result):
       solutions.drawing_styles.get_default_pose_landmarks_style())
   return annotated_image
 
+VisionRunningMode = mp.tasks.vision.RunningMode
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+BaseOptions = mp.tasks.BaseOptions
 
-cap = cv2.VideoCapture(0)
-with mp_pose.Pose(min_detection_confidence=0.3, min_tracking_confidence=0.1) as pose:
-    while cap.isOpened():
-        ret, Image = cap.read()
-        Image = cv2.cvtColor(Image, cv2.COLOR_BGR2RGB)
-        Image.flags.writeable = False
-        results = pose.process(Image)
-        Image.flags.writeable = True
-        Image = cv2.cvtColor(Image, cv2.COLOR_RGB2BGR)  # Re-enable color conversion
-        mp_drawing.draw_landmarks(Image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                   mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=8, circle_radius=1),
-                                   mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=4, circle_radius=1))
-        cv2.imshow('deneme', Image)
+base_options = BaseOptions(model_asset_path='pose_landmarker.task')
+options = PoseLandmarkerOptions(
+    base_options=base_options,
+    output_segmentation_masks=True)
+detector = vision.PoseLandmarker.create_from_options(options)
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+cap = cv2.VideoCapture(1)
+prev_frame_time = 0
+new_frame_time = 0
+
+count = 0
+prev_fps = 0
+font = cv2.FONT_HERSHEY_COMPLEX
+
+while cap.isOpened():
+  ret, image = cap.read()
+  image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+  new_frame_time = time.time()
+  fps = 1 / (new_frame_time - prev_frame_time)
+  prev_frame_time = new_frame_time
+  fps = int(fps)
+  fpss = str(fps)
+  count += 1
+  if not count == 100:
+      prev_fps = prev_fps + fps
+  else:
+      final_fps = prev_fps / 100
+      print(final_fps)
+      count = 0
+      prev_fps = 0
+  detection_result = detector.detect(image)
+  image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+  
+  cv2.putText(image, fpss, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+  
+  cv2.imshow('deneme', image)
+
+  if cv2.waitKey(10) & 0xFF == ord('q'):
+      break
 
 cap.release()
 cv2.destroyAllWindows()
